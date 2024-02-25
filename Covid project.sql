b290f0d1eaeaaad7480eb02c0d575a6aee1827fc
Nimzy2 <117801469+Nimzy2@users.noreply.github.com>
2024-02-26 00:05:36 +0300

    Update Covid project.sql

--- Covid project.sql
+++ Covid project.sql
@@ -1,60 +1,54 @@
 /*
 Covid 19 Data Exploration 
 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
-
+Skills used: Joins, CTE's, Windows Functions, Aggregate Functions, Creating Views,
 */
 
 Select *
-From PortfolioProject..CovidDeaths
-Where continent is not null 
+from `covid-415419.Covid.covid_deaths`
 order by 3,4
 
 
 -- Select Data that we are going to be starting with
 
-Select Location, date, total_cases, new_cases, total_deaths, population
-From PortfolioProject..CovidDeaths
-Where continent is not null 
+select location,date,total_cases,new_cases,total_deaths,population
+from `covid-415419.Covid.covid_deaths`
+where continent is not null
 order by 1,2
 
 
 -- Total Cases vs Total Deaths
 -- Shows likelihood of dying if you contract covid in your country
 
-Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
-Where location like '%states%'
-and continent is not null 
+select location,date,total_cases,total_deaths,(total_deaths/total_cases)*100 as deathpercentage
+from `covid-415419.Covid.covid_deaths`
+where location = 'Kenya'
 order by 1,2
 
 
 -- Total Cases vs Population
 -- Shows what percentage of population infected with Covid
 
-Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
+select location,date,population,total_cases,(total_cases/population)*100 as population_infected_percentage 
+from `covid-415419.Covid.covid_deaths`
 order by 1,2
 
 
 -- Countries with Highest Infection Rate compared to Population
 
-Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-Group by Location, Population
-order by PercentPopulationInfected desc
+select location,population,MAX(total_cases) as Highest_infection,MAX((total_cases/population))*100 as percent_population_infected 
+from `covid-415419.Covid.covid_deaths`
+group by location,population  
+order by percent_population_infected desc
 
 
 -- Countries with Highest Death Count per Population
 
-Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-Where continent is not null 
-Group by Location
-order by TotalDeathCount desc
+select location, Max(total_deaths) as total_death_count
+from `covid-415419.Covid.covid_deaths`
+where continent is not null
+group by location
+order by total_death_count desc
 
 
 
@@ -62,22 +56,20 @@ order by TotalDeathCount desc
 
 -- Showing contintents with the highest death count per population
 
-Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-Where continent is not null 
-Group by continent
-order by TotalDeathCount desc
+select continent, Max(total_deaths) as total_death_count
+from `covid-415419.Covid.covid_deaths`
+where continent is not null
+group by continent
+order by total_death_count desc
 
 
 
 -- GLOBAL NUMBERS
 
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
---Where location like '%states%'
-where continent is not null 
---Group By date
+select date,sum(new_cases) as total_cases, sum(new_deaths) as total_deaths, sum(new_deaths)/sum(new_cases)*100 as global_death_percentange
+from `covid-415419.Covid.covid_deaths`
+where continent is not null
+group by date
 order by 1,2
 
 
